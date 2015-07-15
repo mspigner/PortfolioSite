37c3adba87c461f0b113ac45494be3c22050fd67
mspigner <mspigner@Michelles-MacBook-Pro-2.local>
2015-07-15 02:24:13 -0500

    Intro Load
    Font Changes + Intro loading

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
 // when the page loads, nav to fill screen
+$(document).ready(function(){
+	// // Remove uls and  line
+	$('#Content, #Line, #Title-Load2').addClass('Closed');
+	// // Expand Nav to Fill Screen
+	$('body').addClass('PageLoad');
+	$('#nav-bar').addClass('load');
+	$('#Title-Load').addClass('Open');
 	// Show text - thinker - for 1 second
-	// Clear all text
-	// Show text - designer - for 1 second
-	// Clear all text
-	// Show text - web developer - for 1 second
-	// Clear all text
-	// Show text - strategist - and hold on screen
+	$('.Thinker').delay(500).queue(function(){
+		$(this).addClass('fade-In');
+	});
+	$('.Designer').delay(1500).queue(function(){
+		$(this).addClass('fade-In');
+	});
+	// $('.Developer').delay(2500).queue(function(){
+	// 	$(this).addClass('fade-In');
+	// });
+	$('.Strategist').delay(2500).queue(function(){
+		$(this).addClass('fade-In');
+	});
+	$('body').delay(5000).queue(function(){
+		$('body').removeClass('PageLoad', 'slow');
+		$('header').animate(
+			{height: "112px", opacity:0.6}, 
+			"slow");
+		$('#Content, #Line, #Title-Load2').removeClass('Closed');
+		$('#nav-bar').removeClass('load');
+	});
+	// $('#Content, #Line, #Title-Load2').delay(1000).queue(function(){
+	// 	$(this).removeClass('Closed');
+	// 	$('header').animate({up:50%}, "slow", 200);
+	// });
+});
 
+function addFadeIn(){
+	$(this).addClass('fade-In');
+}
+
+var slideToPosition = $('body').position();
+// $('header').animate({top:"+" + slideToPos.top},200);
 // when user scrolls 
 	// Add class Shrink to Nav (shrink nav, change text and background color)
 	// Fix to top  of the screen
@@ -60,36 +92,41 @@
 $('#Work-Icon').children('p','img').on('click', function (){
 	// $('.Resume').slideUp();
 	// $('.About').slideUp();
-	$('.Work').slideToggle(200);
+	$('.Work').slideToggle(400);
 
 });
 
 $('#Resume-Icon').on('click', function (){
 	// $('.Work').slideUp();
 	// $('.About').slideUp();
-	$('.Resume').slideToggle(200);
+	$('.Resume').slideToggle(400);
 });
 
 $('#About-Icon').on('click', function
 	(){
 	$('.Resume').slideUp();
 	$('Work').slideUp();
-	$('.About').slideToggle(200);
+	$('.About').slideToggle(400);
 });
 
 
 // when the user clicks on the section-text class
-$('Section-li').on('click',function (){
+$('.Section-Text').on('click',function (){
 	// Hide Icon image (class Section-Icon)
 	// Hide Section text (class Section-Title)
-	$(this).addClass('Closed');
-	// Display Div Title (class Section-Div-Title)
-	$(this).find('h1').addClass('Open');
+	$(this).addClass('Closed').delay(2000);
+	$('#Line').addClass('Closed').delay(2000);
+	$('ul, li').addClass('Collapse').delay(2000);
+});
+
+$('.Section h1').on('click', function (){
+	$('.Section-Text').removeClass('Closed');
+	$('#Line').removeClass('Closed', 2000);
+	$('ul, li').removeClass('Collapse', 2000);
+	$('.Section').slideUp(400);
+	$('.Section-Div-Title').hide();
 });
 	
-
-
-
 
 
 
