74e88c8ff51b4bda5a62ef29be5a32101b926c03
mspigner <mspigner@Michelles-MacBook-Pro-2.local>
2015-07-18 10:47:53 -0500

    Updates to Work Css Layout

--- js/work.js
+++ js/work.js
@@ -1,21 +1,69 @@
-// Set up array for all divs
-var projects= ['.BH_OOH','.BH_Wall','.HW','.SpeedStick','.Gatorade','.VictoriaS','.Challenge']
-// // set current position at 0
+
+var projects = [".BH_OOH",".BH_Wall",".HW",".SpeedStick",".Gatorade",".VictoriaS"]
+// set current position at 0
 var currentPosition = 0;
+// var nextDiv = currentPosition-1;
 
 $('#Next').on('click', function (){
-	$('div',projects[currentPosition]).addClass('active');
-	if ($('div').hasClass('.active')){
-		$('div').removeClass('active');
-		$('div').slideUp();
+
+	currentPosition += 1;
+	var previousDiv = currentPosition-1;
+
+	console.log(currentPosition);
+	console.log(previousDiv);
+
+	$('#Previous').prop('disabled',false);
+
+	$(projects[currentPosition]).delay(1).queue(function(){
+		$(projects[currentPosition]).addClass('slideIn');
+	});
+	$(projects[previousDiv]).removeClass('slideIn');
+
+	console.log(projects[previousDiv]);
+
+	if (currentPosition === projects.length-1) {
+		$('#Next').prop('disabled', true);
 	}
-	// else {
-	// 	$('div').removeClass('active');
-	// 	currentPosition += 1;
-	// 	$('div',projects[currentPosition]).slideDown();
-	// }
-	
+
 });
+
+// When the user clicks previous
+$('#Previous').on('click', function (){
+	// Enable the next button
+	$('#Next').prop('disabled', false);
+	// Subtract 1 from current position
+	currentPosition -= 1;
+	// Display project that corresponds with the current position
+	$(projects[currentPosition]).addClass('slideIn');
+	// Add 1 to the current position
+	var nextDiv = currentPosition+1;
+	// Hide the project that correspondes ot the current position +1
+	$(projects[nextDiv]).removeClass('slideIn');
+	// if the current position is equal to 0
+	if (currentPosition === 0) {
+		// Disable the previous button
+		$('#Previous').prop('disabled',true);
+	}
+});
+
+// $('#Previous').on('click', function (){
+
+// 	currentPosition -= 1;
+// 	var nextDiv = currentPosition+1;
+
+// 	$('#Next').prop('disabled', false);
+// 	$(projects[currentPosition]).delay(1).queue(function(){
+// 		$(projects[currentPosition]).addClass('slideIn');
+// 	});
+
+// 	$(projects[nextDiv]).removeClass('slideIn');
+	
+// 	if (currentPosition === 0 ) {
+// 		$('#Previous').prop('disabled', true);
+// 	}
+
+// });
+
 //when the user clicks on next
 // $('#Next').on('click',function (){
 // //store the current project number
